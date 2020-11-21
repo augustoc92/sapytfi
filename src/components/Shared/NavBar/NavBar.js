c7d4f77ef5109bbf6bbda90d96dd95a700e790de
Augusto Camiletti <augusto.camiletti@learnosity.com>
2020-11-21 17:32:46 -0300

    Router in navbar

--- src/components/Shared/NavBar/NavBar.js
+++ src/components/Shared/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import { Menu } from 'antd';
+import { Link } from "react-router-dom";
 import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import React from 'react';
 
@@ -19,26 +20,22 @@ class NavBar extends React.Component {
     return (
       <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal" theme='dark'>
         <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
+          <Link to="/Home">
+          Novedades
+          </Link>
         </Menu.Item>
         <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-          Navigation Two
+          Aulas
         </Menu.Item>
-        <SubMenu icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
+        <SubMenu icon={<SettingOutlined />} title="Administracion">
+          <Menu.ItemGroup title="Cursado">
+            <Menu.Item key="setting:1">Carrera</Menu.Item>
+            <Menu.Item key="setting:2">Materias</Menu.Item>
           </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          <Menu.ItemGroup title="Ciclo Lectivo">
+            <Menu.Item key="setting:3">Calendario Academicos</Menu.Item>
           </Menu.ItemGroup>
         </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
       </Menu>
     );
   }
